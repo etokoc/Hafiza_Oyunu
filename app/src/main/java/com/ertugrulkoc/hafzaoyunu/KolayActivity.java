e36b184d53b530fa69f8d6c5542a3e8800a93839
Ertuğrul KOÇ <etoertugrul.koc@gmail.com>
2021-06-14 02:14:07 +0300

    commit

--- app/src/main/java/com/ertugrulkoc/hafzaoyunu/KolayActivity.java
+++ app/src/main/java/com/ertugrulkoc/hafzaoyunu/KolayActivity.java
@@ -5,18 +5,27 @@ import android.util.Log;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class KolayActivity extends AppCompatActivity implements View.OnClickListener {
     ImageButton b1;
     List<Integer> images;
     List<ImageButton> buttons;
     GridLayout gridLayout;
+    List<Integer> rnd_list;
+    List<ImageButton> usesButtons;
+    List<Integer> usesImages;
+    int tiklamaAdet = 0;
+    int sayi;
+    List<Integer> varolanlar;
+    Random rnd;
+    List<Integer> uretilenler;
+    int c = 0;
     private Object childView;
 
     @Override
@@ -24,13 +33,40 @@ public class KolayActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kolay);
         b1 = findViewById(R.id.image1);
+        varolanlar = new ArrayList<>();
+        usesImages = new ArrayList<>();
+        usesButtons = new ArrayList<>();
+        rnd_list = new ArrayList<>();
         gridLayout = findViewById(R.id.kolay_gridView);
         images = new ArrayList<>();
         buttons = new ArrayList<>();
         animalInitilatizions();
         buttonInitilatizions();
-        listenerInitilatizion();{
+        rnd = new Random();
+        uretilenler = new ArrayList<>();
+        createRandomNumber();
+        buttonImageAdd();
+        listenerInitilatizion();
+
+    }
+
+    private void createRandomNumber() {
+        for (int i = 0; i < 6; i++) {
+            sayi = rnd.nextInt(3);
+            while (varolanlar.contains(sayi)) {
+                sayi = rnd.nextInt(3);
+            }
+            if (varolanlar.size()<4){
+                if (!varolanlar.contains(sayi)) {
+                    varolanlar.add(sayi);
+                }
+            }else {
+                uretilenler=varolanlar;
+                varolanlar.clear();
+            }
         }
+
+        Log.i("uretilen", "" + varolanlar);
     }
 
     private void listenerInitilatizion() {
@@ -48,14 +84,42 @@ public class KolayActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
+    private void cardClosing() {
+        for (int i = 0; i < buttons.size(); i++) {
+            buttons.get(i).setImageResource(R.drawable.brain);
+        }
+    }
+
     private void animalInitilatizions() {
         images.add(R.drawable.bee);
         images.add(R.drawable.horse);
         images.add(R.drawable.chicken);
     }
 
+    private void buttonImageAdd() {
+        for (int i = 0; i < buttons.size(); i++) {
+           // buttons.get(i).setImageResource(images.get(uretilenler.get(i)));
+        }
+
+    }
+
     @Override
     public void onClick(View view) {
-        Toast.makeText(this, "12312", Toast.LENGTH_SHORT).show();
+        if (tiklamaAdet < 2) {
+            for (int i = 0; i < buttons.size(); i++) {
+                if (view instanceof ImageButton) {
+                    if (view.getId() == buttons.get(i).getId()) {
+                        //    buttons.get(i).setImageResource(images.get(createRandomNumber()));
+                        //  tiklamaAdet++;
+                    }
+                }
+            }
+        } else {
+            cardClosing();
+            tiklamaAdet = 0;
+        }
     }
+
+
 }
+
