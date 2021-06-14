3ceac36380ca39e1093c19203b0fc775220195ba
Ertuğrul KOÇ <etoertugrul.koc@gmail.com>
2021-06-14 14:09:15 +0300

    commit

--- app/src/main/java/com/ertugrulkoc/hafzaoyunu/KolayActivity.java
+++ app/src/main/java/com/ertugrulkoc/hafzaoyunu/KolayActivity.java
@@ -9,41 +9,33 @@ import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Random;
 
 public class KolayActivity extends AppCompatActivity implements View.OnClickListener {
-    ImageButton b1;
     List<Integer> images;
     List<ImageButton> buttons;
     GridLayout gridLayout;
-    List<Integer> rnd_list;
-    List<ImageButton> usesButtons;
-    List<Integer> usesImages;
     int tiklamaAdet = 0;
     int sayi = 0;
     List<Integer> varolanlar;
     Random rnd;
-    List<Integer> uretilenler;
-    int c = 0;
+    Hashtable<ImageButton, Integer> buttonVeri;
     private Object childView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kolay);
-        b1 = findViewById(R.id.image1);
+        buttonVeri = new Hashtable<>();
         varolanlar = new ArrayList<>();
-        usesImages = new ArrayList<>();
-        usesButtons = new ArrayList<>();
-        rnd_list = new ArrayList<>();
         gridLayout = findViewById(R.id.kolay_gridView);
         images = new ArrayList<>();
         buttons = new ArrayList<>();
         animalInitilatizions();
         buttonInitilatizions();
         rnd = new Random();
-        uretilenler = new ArrayList<>();
         sayi = 0;
         createRandomNumber();
         buttonImageAdd();
@@ -93,12 +85,14 @@ public class KolayActivity extends AppCompatActivity implements View.OnClickList
 
     private void buttonImageAdd() {
         for (int i = 0; i < buttons.size(); i++) {
-            if (i<=2){
-                buttons.get(i).setImageResource(images.get(varolanlar.get(i)));
-            }else {
+            if (i <= 2) {
+//                buttons.get(i).setImageResource(images.get(varolanlar.get(i)));
+                buttonVeri.put(buttons.get(i), images.get(varolanlar.get(i)));
+            } else {
                 createRandomNumber();
-                for (int a = i; a<buttons.size();a++){
-                    buttons.get(a).setImageResource(images.get(varolanlar.get(a-3)));
+                for (int a = i; a < buttons.size(); a++) {
+//                    buttons.get(a).setImageResource(images.get(varolanlar.get(a - 3)));
+                    buttonVeri.put(buttons.get(a), images.get(varolanlar.get(a - 3)));
                 }
                 break;
             }
@@ -108,24 +102,26 @@ public class KolayActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View view) {
-//        if (tiklamaAdet < 2) {
-//            for (int i = 0; i < buttons.size(); i++) {
-//                if (view instanceof ImageButton) {
-//                    if (view.getId() == buttons.get(i).getId()) {
-//                        if (i<3){
-//                            buttons.get(i).setImageResource(images.get(varolanlar.get(i)));
-//                        }else {
-//                            createRandomNumber();
-//                            buttons.get(i).setImageResource(images.get(varolanlar.get(i-3)));
-//                        }
-//                        //  tiklamaAdet++;
-//                    }
-//                }
-//            }
-//        } else {
-//            cardClosing();
-//            tiklamaAdet = 0;
-//        }
+        if (tiklamaAdet < 2) {
+            for (int i = 0; i < buttons.size(); i++) {
+                if (view instanceof ImageButton) {
+                    if (view.getId() == buttons.get(i).getId()) {
+                        tiklamaAdet++;
+                        ((ImageButton) view).setImageResource(buttonVeri.get(buttons.get(i)));
+                    }
+                }
+            }
+        } else {
+            checkCardImage();
+            cardClosing();
+            tiklamaAdet = 0;
+        }
+    }
+
+    private void checkCardImage() {
+        for (int i = 0; i<buttons.size();i++){
+
+        }
     }
 
 
