e4f6c9499ca521309c78e5d95a00a3768f10127d
Ertuğrul KOÇ <etoertugrul.koc@gmail.com>
2021-06-17 11:07:28 +0300

    veritabanli

--- app/src/main/java/com/ertugrulkoc/hafzaoyunu/Skorlar.java
+++ app/src/main/java/com/ertugrulkoc/hafzaoyunu/Skorlar.java
@@ -1,23 +1,47 @@
 package com.ertugrulkoc.hafzaoyunu;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Skorlar extends AppCompatActivity {
 
+    String birinci,ikinci,ucuncu;
+    int first, second, third;
+    int hamleSayisi;
+    TextView view_birinci,view_ikinci,view_ucuncu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skorlar);
 
+        view_birinci = findViewById(R.id.txt_birinci);
+        view_ikinci = findViewById(R.id.txt_ikinci);
+        view_ucuncu = findViewById(R.id.txt_ucuncu);
+
         DataHelper dataHelper = new DataHelper(this.getApplicationContext());
-        Cursor cursor= dataHelper.veriListele();
+        Cursor cursor = dataHelper.veriListele();
+        first = 100;
+        second = 100;
+        third = 100;
         while (cursor.moveToNext()){
-            Toast.makeText(this, ""+cursor.getString(0)+" -- "+cursor.getString(1)+" -- "+cursor.getString(2), Toast.LENGTH_SHORT).show();
+            if (Integer.parseInt(cursor.getString(1)) < first) {
+                first = Integer.parseInt(cursor.getString(1));
+                birinci="Hamle Sayısı: "+cursor.getString(1)+" Mod: "+cursor.getString(0)+" Tarih: "+cursor.getString(2);
+            } else if (Integer.parseInt(cursor.getString(1)) < second) {
+                second = Integer.parseInt(cursor.getString(1));
+                ikinci="Hamle Sayısı: "+cursor.getString(1)+" Mod: "+cursor.getString(0)+" Tarih: "+cursor.getString(2);
+            } else if (Integer.parseInt(cursor.getString(1)) < third) {
+                third = Integer.parseInt(cursor.getString(1));
+                ucuncu="Hamle Sayısı: "+cursor.getString(1)+" Mod: "+cursor.getString(0)+" Tarih: "+cursor.getString(2);}
         }
+
+        view_birinci.setText(birinci);
+        view_ikinci.setText(ikinci);
+        view_ucuncu.setText(ucuncu);
 
 
 
